80351d8b9898fd19760dae84b88308d10074b23d
panlingnan <panlingnan@bytedance.com>
2026-06-16 15:13:19 +0800

    xx

--- terraform-project/variables.tf
+++ terraform-project/variables.tf
@@ -38,5 +38,5 @@ variable "iam_users" {
     offboarding_group       = optional(string, "")
   }))
   description = "IAM 用户列表，用于用户全生命周期管理（入职/转岗/离职）"
-  default     = [] #...
+  default     = [] #....
 }
